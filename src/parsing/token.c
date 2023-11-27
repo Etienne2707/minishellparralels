aa3d9a3aa0004c0158371de6b95ba3c2ef61a5ae
Minh Anh LE DUC <minhanh_leduc@outlook.fr>
2023-11-27 09:08:51 +0100

    fixed exit error msg

--- src/parsing/token.c
+++ src/parsing/token.c
@@ -6,7 +6,7 @@
 /*   By: educlos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 17:13:14 by educlos           #+#    #+#             */
-/*   Updated: 2023/11/27 08:53:06 by mle-duc          ###   ########.fr       */
+/*   Updated: 2023/11/27 09:08:37 by mle-duc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,6 +88,6 @@ int	init_struct(char **cmd, t_token *token, t_pars **pars)
 		i++;
 	}
 	free_token(&token, i);
-	print_list(pars);
+//	print_list(pars);
 	return (1);
 }
