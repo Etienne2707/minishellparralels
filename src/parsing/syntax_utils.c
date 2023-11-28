2ce9cb234035649a5b43d99a470355a7ddb0cd8a
Minh Anh LE DUC <minhanh_leduc@outlook.fr>
2023-11-28 18:27:02 +0100

    fix replace whitespaces function

--- src/parsing/syntax_utils.c
+++ src/parsing/syntax_utils.c
@@ -6,7 +6,7 @@
 /*   By: mle-duc <mle-duc@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 12:11:50 by mle-duc           #+#    #+#             */
-/*   Updated: 2023/11/28 17:23:14 by mle-duc          ###   ########.fr       */
+/*   Updated: 2023/11/28 18:26:25 by mle-duc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,14 @@ void	syntax_quote2(char *str, int *s, int *d, int *i)
 		syntax2(str, d, i);
 }
 
+static void	index_in_quotes(char *dest, char c, int *i)
+{
+	(*i)++;
+	while (dest[*i] != c && dest[*i] != 0)
+		(*i)++;
+	(*i)++;
+}
+
 void	replace_whitespaces(char *dest)
 {
 	int	i;
@@ -51,6 +59,15 @@ void	replace_whitespaces(char *dest)
 	i = 0;
 	while (dest[i])
 	{
+		while (dest[i] == 34 || dest[i] == 39)
+		{
+			if (dest[i] == 34)
+				index_in_quotes(dest, 34, &i);
+			else if (dest[i] == 39)
+				index_in_quotes(dest, 39, &i);
+		}
+		if (dest[i] == 0)
+			return ;
 		if (dest[i] >= 9 && dest[i] <= 13)
 			dest[i] = 32;
 		i++;
